b0ec5efb02dd19f92aa5d23394f6d86e820079a7
rakeshmadu <rakeshmadu24.com>
2022-11-28 09:25:39 +0530

    updated with comments

--- src/main/java/com/employee/roll/controller/EmpController.java
+++ src/main/java/com/employee/roll/controller/EmpController.java
@@ -3,8 +3,6 @@ package com.employee.roll.controller;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,36 +17,36 @@ import com.employee.roll.service.EmpIService;
 @RestController
 
 public class EmpController {
-	@Autowired
+	@Autowired //The @Autowired annotation marks a Constructor, Setter method, Properties and Config() method as to be autowired that is ‘injecting beans'(Objects) at runtime by Spring Dependency Injection mechanism 
 	EmpIService empService;
 
-	@DeleteMapping("/delete/{empid}")
+	@DeleteMapping("/delete/{empid}") // deletes employ data by id 
 	public Optional<EmpModel> deleteEmp(@PathVariable int empid) {
 		Optional<EmpModel> empModel = empService.delete(empid);
 		System.out.println("employee deleted");
 		return empModel;
 	}
 
-	@GetMapping("/findid/{empid}")
+	@GetMapping("/findid/{empid}") // find employee data by id
 	public Optional<EmpModel> getById(@PathVariable int empid) {
 		System.out.println("id found :" + empid);
 		Optional<EmpModel> searchId = empService.getid(empid);
 		return searchId;
 	}
 
-	@PutMapping("/update/{empid}")
+	@PutMapping("/update/{empid}") //update data of employ
 	public ResponseEntity updateUser(@RequestBody EmpModel emp, @PathVariable int empid) {
 		EmpModel empUpdate = empService.update(emp, empid);
 		return new ResponseEntity(empUpdate, "employee updated successfully");
 	}
 
-	@PostMapping("/register")
+	@PostMapping("/register") // registering new employ
 	public ResponseEntity registerEmploy(@RequestBody EmpModel employ) {
 		EmpModel empSave = empService.register(employ);
 		return new ResponseEntity(empSave, "Registered successfully");
 	}
 
-	@GetMapping("/getAll")
+	@GetMapping("/getAll") // displays all employee details
 	public List<EmpModel> getAllEmploy() {
 
 		return empService.getAllemploy();
